af6904bd49f481694a25818f7a5c6ac238e16c9e
optitudes <optt.itudes@gmail.com>
2025-06-10 13:38:14 -0500

    fix: elimina la lista de municipios del dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    @php
-        $municipios = session('municipios');
-    @endphp
 
-    @if ($municipios)
-        <ul>
-            @foreach ($municipios as $municipio)
-                <li>{{ $municipio->nombre }}</li>
-            @endforeach
-        </ul>
-    @endif
 @endsection
