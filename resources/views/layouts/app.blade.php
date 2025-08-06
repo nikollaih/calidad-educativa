4dcac25b06ffbc4e09577d8605eddef07c64375e
Juan Sebastian Rojas Garcia <optt.itudes@gmail.com>
2025-08-05 23:54:40 -0500

    elimina viejo componente de pmi en layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -150,12 +150,6 @@
                             </a>
                             <ul class="menu-sub">
                                 <li class="menu-item">
-                                    <a   class="menu-link">
-                                        <i class="menu-icon fas fa-bullseye"></i>
-                                        <div data-i18n="PMI - Metas"> PMI - Metas</div>
-                                    </a>
-                                </li>
-                                <li class="menu-item">
                                     <a href="{{ url('municipios')}}" class="menu-link">
                                         <i class="menu-icon fas fa-map"></i>
                                         <div data-i18n="Municipios"> Municipios</div>
