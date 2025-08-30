42d67f52684d828153ec86b388c966951447b57c
Cristian Palacio <cristiancamilopala@gmail.com>
2025-08-30 18:25:42 -0500

    Fix: Arregla migracion de agregar responsable de proyectos transversales

---
+++ database/migrations/2025_08_30_181132_add_responsable_proyecto.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Enums\PamEstadoEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('proyectos_transversales', function (Blueprint $table) {
+            $table->unsignedBigInteger('representante_id')->nullable()->after('institucion_id');
+
+            $table->foreign('representante_id')->references('id')->on('users')->onDelete('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('proyectos_transversales', function (Blueprint $table) {
+            $table->dropForeign(['representante_id']);
+            $table->dropColumn('representante_id');
+        });
+    }
+};
