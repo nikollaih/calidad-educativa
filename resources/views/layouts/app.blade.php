271ad6cbdffa012981664fb152afde2f81ccd573
Juan Sebastian Rojas Garcia <optt.itudes@gmail.com>
2025-11-23 18:39:05 -0500

    corrige error al acceso al perfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -283,7 +283,7 @@
                                         </a>
                                         <ul class="dropdown-menu dropdown-menu-end">
                                             <li>
-                                                <a class="dropdown-item" href="profile">
+                                                <a class="dropdown-item" href="{{asset('profile')}}">
                                                     <div class="d-flex">
                                                         <div class="flex-shrink-0 me-3">
                                                             <div class="avatar avatar-online">
